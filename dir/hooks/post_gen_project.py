875a1ba056586ddd76587257bcd58f737732bae2
Jens Hübner <jhuebner@nano.uni-hannover.de>
2021-02-07 16:59:09 +0100

    forgot cookiecutter.<value>

--- dir/hooks/post_gen_project.py
+++ dir/hooks/post_gen_project.py
@@ -2,7 +2,7 @@ import subprocess
 
 # Create git repo.
 subprocess.call(['git', 'init'])
-subprocess.call(["git", "config", "user.name" ,"{{ author_name }}"])
-subprocess.call(["git", "config", "user.email" ,"{{ author_email }}"])
+subprocess.call(["git", "config", "user.name", "{{ cookiecutter.author_name }}"])
+subprocess.call(["git", "config", "user.email", "{{ cookiecutter.author_email }}"])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
